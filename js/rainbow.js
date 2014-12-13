71777d35888eaad86c100052ac7cade60f3f7829
Chris Russell-Walker <chrisrussell-walker@Chriss-MacBook-Pro-2.local>
2014-12-13 17:11:19 -0500

    Reverting to older version santa

--- js/rainbow.js
+++ js/rainbow.js
@@ -1,100 +1,65 @@
 
 $(document).ready(function() {
   var canvas = document.getElementById("game"),
-      ctx = canvas.getContext("2d"),
-      width = 800,
-      height = 400,
-      player = {
-        x : width/2,
-        y : height - 5,
-        width : 5,
-        height : 5,
-        speed: 5,
-        velX: 0,
-        velY: 0,
-        jumping: true
-      },
-      keys = [],
-      friction = 0.8,
-      gravity = 0.2;
+      ctx = canvas.getContext("2d");
   
-  canvas.height = $("#game").height();
+  canvas.height = 700;
   canvas.width = $("#game").width();
-   
-  function update(){
-    
-    // check keys
-      if (keys[38] || keys[32]) {
-          // up arrow or space
-        if(!player.jumping){
-         player.jumping = true;
-         player.velY = -player.speed*2;
-        }
-      }
-      if (keys[39]) {
-          // right arrow
-          if (player.velX < player.speed) {             
-              player.velX++;         
-           }     
-      }     
-      if (keys[37]) {         
-          // left arrow      
-          if (player.velX > -player.speed) {
-              player.velX--;
-          }
-      }
-   
-      player.velX *= friction;
-   
-      player.velY += gravity;
-   
-      player.x += player.velX;
-      player.y += player.velY;
-   
-      if (player.x >= width-player.width) {
-          player.x = width-player.width;
-      } else if (player.x <= 0) {         
-          player.x = 0;     
-      }    
-    
-      if(player.y >= height-player.height){
-          player.y = height - player.height;
-          player.jumping = false;
-      }
-   
-    //ctx.fillStyle = "red";
-    //ctx.fillRect(player.x, player.y, player.width, player.height);
-    var imageObj = new Image();
-      imageObj.onload = function() { ctx.drawImage(imageObj, player.x, player.y); };
-      if (time % 2) {
-        imageObj.src = 'http://png-4.findicons.com/files/icons/1278/quickpix_2006/48/santa_sprite.png';
-      } else {
-        imageObj.src = 'http://png-4.findicons.com/files/icons/1278/quickpix_2006/48/santa_sprite.png';
-      }
-     //ctx.clearRect(0,0,width,height);
 
-    // ctx.fillStyle = 'skyblue';
-      //ctx.fillRect(0, 0, canvas.width, canvas.height);
+  var mySanta = {
+      x: 600,
+      y: 600,
+      speed: 400,
+  };
    
-    requestAnimationFrame(update);
+  var keysDown = {};
+  window.addEventListener('keydown', function(e) {
+      keysDown[e.keyCode] = true;
+  });
+  window.addEventListener('keyup', function(e) {
+      delete keysDown[e.keyCode];
+  });
+   
+  function update(mod) {
+      if (37 in keysDown) {
+          mySanta.x -= mySanta.speed * mod;
+      }
+      while (32 in keysDown) {
+          mySanta.x -= mySanta.speed * 2;
+      }
+      //if (38 in keysDown) {
+         // mySanta.y -= mySanta.speed * mod;
+      //}
+      if (39 in keysDown) {
+          mySanta.x += mySanta.speed * mod;
+      }
+      //if (40 in keysDown) {
+        //  mySanta.y += mySanta.speed * mod;
+     // }
   }
-
+   
   function render() {
-      
-  }
-  document.body.addEventListener("keydown", function(e) {
-      keys[e.keyCode] = true;
-  });
-   
-  document.body.addEventListener("keyup", function(e) {
-      keys[e.keyCode] = false;
-  });
-   
-  window.addEventListener("load",function(){
-      update();
-  });
-  var time = Date.now();
+      var imageObj = new Image();
+      imageObj.onload = function() { ctx.drawImage(imageObj, mySanta.x, mySanta.y); };
+      if (time % 2) {
+        imageObj.src = 'http://www.sumoware.com/images/temp/xztdjaomnsokljct.png';
+      } else {
+        imageObj.src = 'http://www.sumoware.com/images/temp/xzhscmemgbsnrjrc.png';
+      }
+      ctx.fillStyle = 'skyblue';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  }
+
+  function run() {
+      update((Date.now() - time) / 1000);
+      render();
+      time = Date.now();
+  }
+   
+  var time = Date.now();
+  setInterval(run, 20);
+   
   /* Our random score generator */
   var UIScore = $('#score');
   var score = 0;
